a1c1cc90a1a9b095009ef6f9d8e85b54c882ee08
Bruno Barbosa <brb.dev@outlook.com>
2021-12-30 22:52:49 -0400

    jwt

--- src/routes/authorization.route.ts
+++ src/routes/authorization.route.ts
@@ -1,34 +1,36 @@
 import { Router, Request, Response, NextFunction } from 'express';
+import  JWT from 'jsonwebtoken';
+import { StatusCodes } from 'http-status-codes';
+import basicAuthenticationMiddleware from '../middlewares/basic-authentication.middleware';
 import ForbiddenError from '../errors/forbidden.error';
-import userRepository from '../repositories/user.repository';
 
 const authorizationRoute = Router();
 
-authorizationRoute.post('/token', async (req: Request, res: Response, next: NextFunction) => {
+// “iss” O domínio da aplicação geradora do token
+// “sub” É o assunto do token, mas é muito utilizado para guarda o ID do usuário
+// “aud” Define quem pode usar o token
+// “exp” Data para expiração do token
+// “nbf” Define uma data para qual o token não pode ser aceito antes dela
+// “iat” Data de criação do token
+// “jti” O id do token
+
+authorizationRoute.post('/token', basicAuthenticationMiddleware, async (req: Request, res: Response, next: NextFunction) => {
 
 	try {
 
-		const authorizationHeader = req.headers['authorization'];
-	
-		if (!authorizationHeader) {
-			throw new ForbiddenError('Credenciais não informadas');
+		const user = req.user;
+
+		if (!user) {
+			throw new ForbiddenError('Usuário não informado');
+			
 		}
 
-		const [authenticationType, token] = authorizationHeader.split(' ');
+		const jwtPayload = {username: user.username};
+		const jwtOptions = {subject: user?.uuid};
+		const secretKey = 'my_secret_key';
 
-		if (authenticationType !== 'Basic' || !token) {
-			throw new ForbiddenError('Tipo de autenticação inválido');
-		}
-
-		const tokenContent = Buffer.from(token, 'base64').toString('utf-8');
-
-		const [username, password] = tokenContent.split(':');
-
-		if (!username || !password) {
-			throw new ForbiddenError('Credenciais não preenchidas');
-		}
-
-		const user = await userRepository.findByUsernameAndPassword(username, password);
+		const jwt = JWT.sign(jwtPayload, secretKey, jwtOptions);
+		res.status(StatusCodes.OK).json({token: jwt});
 
 	} catch (error) {
 		next(error);
